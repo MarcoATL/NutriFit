95db7158af0fc8786bf28de745982ed7186ad481
MarcoATL <marko274.matl@gmail.com>
2023-06-11 15:56:15 -0600

    pantallas comida

---
+++ js/Comida.js
@@ -0,0 +1,44 @@
+const Platillos = [
+    { name: "Pollo rostizado", image: "./img/comida-1.jpg" },
+    { name: "Pozole", image: "./img/comida-2.jpg" },
+    { name: "Pollo empanizado", image: "./img/comida-3.png" },
+    { name: "Ensalada griega", image: "./img/comida-4.jpg" },
+    { name: "Tacos al pastor", image: "./img/comida-5.jpg" },
+    { name: "Tostadas de tinga", image: "./img/comida-6.jpg" },
+  ];  
+
+//Platillos dinamicos
+const cardContainer = document.querySelector(".comida:last-child");
+
+Platillos.forEach((card) => {
+  const cardHTML = `
+    <div class="col-md-4 col-sm-4">
+        <div class="card dish-card" data-name="${card.name}">
+            <img src="${card.image}" alt="Imgane Comida" class="card-img-top img-platillo">
+            <div class="card-body">
+            <h5 class="card-title">${card.name}</h5>
+            <a href="#" class="btn ver-receta">Ver receta</a>
+            </div>
+        </div>
+    </div>
+  `;
+  cardContainer.insertAdjacentHTML("beforeend", cardHTML);
+});
+
+//Buscador
+const searchInput = document.getElementById('searchInput');
+const dishCards = document.querySelectorAll('.dish-card');
+
+searchInput.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+
+    dishCards.forEach(function(card) {
+        const dishName = card.dataset.name.toLowerCase();
+
+        if (dishName.includes(searchTerm)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
